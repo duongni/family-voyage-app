6204ad32042bb15b7b6bff10fad264bfbbfb47ea
duongni <133175915+duongni@users.noreply.github.com>
2023-07-08 22:20:58 -0400

    added npm react router for Home and SearchPage components

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import SearchBar from "./SearchBar";
 import Footer from "./Footer";
-
+import SearchPage from "./SearchPage";
 import Home from "./Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <div className="header">
-        <SearchBar />
-      </div>
-      <Home />
-      <Footer />
+      <Router>
+        <div className="header">
+          <SearchBar />
+        </div>
+
+        <Routes>
+          <Route path="/search" element={<SearchPage />} />
+
+          <Route path="/" element={<Home />} />
+        </Routes>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
