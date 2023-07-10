96d71eb8a03e3921c174ebc12275fbf43f6acb70
duongni <133175915+duongni@users.noreply.github.com>
2023-07-09 23:41:49 -0400

    Added styling to SearchPage component

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -7,6 +7,11 @@ export default function SearchPage() {
       <div className="SearchPage_info">
         <p>25 stays . July 26 to August 01 . 2 adult and 1 child</p>
         <h1>Family's Favorites</h1>
+        <button>Cancelation Flexibility</button>
+        <button>Type of Place</button>
+        <button>Price</button>
+        <button>Rooms and Beds</button>
+        <button>More Filters</button>
       </div>
     </div>
   );
