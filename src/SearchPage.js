bf814c79f5062b287c6f13ed1b8a8e277e21a015
duongni <133175915+duongni@users.noreply.github.com>
2023-07-09 22:34:59 -0400

    Make the Route works and working on SearchPage component

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,5 +1,13 @@
 import React from "react";
+import "./SearchPage.css";
 
 export default function SearchPage() {
-  return <div>Hello</div>;
+  return (
+    <div className="SearchPage">
+      <div className="SearchPage_info">
+        <p>25 stays . July 26 to August 01 . 2 adult and 1 child</p>
+        <h1>Family's Favorites</h1>
+      </div>
+    </div>
+  );
 }
