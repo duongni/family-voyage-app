15db0dc7c9484f4ee5e7e51c58631b160e94668c
duongni <133175915+duongni@users.noreply.github.com>
2023-07-11 23:22:19 -0400

    Used useNavigate from react router to direct to search page

--- src/Banner.js
+++ src/Banner.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./Banner.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Banner() {
+  const navigate = useNavigate();
   return (
     <div className="Banner">
       <div className="banner-info">
         <h1>Get out and make memories together</h1>
         <h5>Plan a differen kind of getaway for the whole family.</h5>
-        <button>Explore Family's Favorites</button>
+        <button onClick={() => navigate("/search")}>
+          Explore Family's Favorites
+        </button>
       </div>
     </div>
   );
