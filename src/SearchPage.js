119c1a16dae544e3cd0825ae6d47cd57ffe2b77d
duongni <133175915+duongni@users.noreply.github.com>
2023-07-11 21:22:56 -0400

    Adding information to Search Result componet

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -6,7 +6,7 @@ export default function SearchPage() {
   return (
     <div className="SearchPage">
       <div className="SearchPage_info">
-        <p>25 stays . July 26 to August 01 . 2 adult and 1 child</p>
+        <p>25 stays · August 2 to August 5 · 2 adult and 2 child</p>
         <h1>Family's Favorites</h1>
         <button>Cancelation Flexibility</button>
         <button>Type of Place</button>
@@ -14,7 +14,15 @@ export default function SearchPage() {
         <button>Rooms and Beds</button>
         <button>More Filters</button>
       </div>
-      <SearchResult />
+      <SearchResult
+        img="https://images.trvl-media.com/lodging/1000000/20000/19600/19588/ab900421.jpg?impolicy=resizecrop&rw=1200&ra=fit"
+        location="Hilton Rose Hall Resort and Spa"
+        title="Ocean View -1 King bed"
+        description="4 guest · 1 bedroom · 1 bed · 1 bathroom · Wifi · Kitchen · Free parking · All-inclusive · Ocean view"
+        star={4.3}
+        price="$400 / night"
+        total="$1200 total"
+      />
     </div>
   );
 }
--- src/SearchResult.js
+++ src/SearchResult.js
@@ -1,6 +1,40 @@
 import React from "react";
 import "./SearchResult.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function SearchResult() {
-  return <div className="SearchResult">Hello</div>;
+export default function SearchResult({
+  img,
+  location,
+  title,
+  description,
+  star,
+  price,
+  total,
+}) {
+  return (
+    <div className="SearchResult">
+      <img src={img} alt="" />
+      <FontAwesomeIcon icon="fa-light fa-heart" className="heart" />
+      <div className="SearchResult-info">
+        <div className="SearchResult-info-top">
+          <p>{location}</p>
+          <h3>{title}</h3>
+          <br />
+          <p>{description}</p>
+        </div>
+        <div className="SearchResult-info-bottom">
+          <div className="SearchResult-star">
+            <p>
+              <FontAwesomeIcon icon="fa-sharp fa-solid fa-star" />
+              {star}
+            </p>
+          </div>
+          <div className="SearchResult-price">
+            <h2>{price}</h2>
+            <p>{total}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
